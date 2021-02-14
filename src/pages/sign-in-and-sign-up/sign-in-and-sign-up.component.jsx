ab2896c616a6d8b23a32edb4bdf4d13705878e02
Aaron Waguespack <Aaron.Paul.Waguespack@gmail.com>
2021-02-14 12:43:55 -0700

    sign in & sign up

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUp = () => {
+
+    return (
+    <div className="sign-in">Sign-In</div>
+    )
+};
+
+export default SignInAndSignUp;
