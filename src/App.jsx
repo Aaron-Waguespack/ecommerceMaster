05db3461a38d469d557e8cf9d87f8f0a2410c5e1
Aaron Waguespack <46909858+Aaron-Waguespack@users.noreply.github.com>
2021-02-17 14:56:37 -0700

    sign out fix

--- src/App.jsx
+++ src/App.jsx
@@ -25,17 +25,18 @@ class App extends Component{
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
-        userRef.onSnapshot(
-          snapShot => {
-            this.setState({
-              currentUser:{
-                id:snapShot.id,
-                ...snapShot.data()
-              }
-            });
-            console.log(this.state)
-          }
-        )
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser:{
+              id:snapShot.id,
+              ...snapShot.data()
+            }
+          },
+            () => {console.log(this.state)}
+          );
+      })
+      } else {
+        this.setState({currentUser:userAuth})
       }
     });
   }
