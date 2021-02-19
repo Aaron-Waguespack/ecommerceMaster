3120ff9bf59f7e78c97225bfb55a3c5bf8c29f48
Aaron Waguespack <Aaron.Paul.Waguespack@gmail.com>
2021-02-18 17:09:24 -0700

    finishing sign in onSubmit

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import CustomButton from '../custom-button/custom-button.component.jsx';
 import FormInput from '../form-input/form-input.component.jsx';
 import './sign-in.styles.scss';
 
-import {signInWithGoogle} from '../../firebase/firebase.utilites.jsx';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utilites.jsx';
 
 class SignIn extends Component{
     constructor(props){
@@ -14,9 +14,17 @@ class SignIn extends Component{
         }
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async(event) => {
         event.preventDefault();
-        this.setState({email:"", password: ""})
+        const {email, password} = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'', password:''});
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     handleChange = (event) => {
