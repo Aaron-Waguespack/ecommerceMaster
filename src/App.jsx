1102338f45c953dbb452976745776c2a93ecc0b2
Aaron Waguespack <Aaron.Paul.Waguespack@gmail.com>
2021-02-18 17:02:41 -0700

    implemented firebase email and password auth

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ class App extends Component{
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
-        
+
         userRef.onSnapshot(snapShot => {
           this.setState({
             currentUser:{
@@ -33,6 +33,7 @@ class App extends Component{
               ...snapShot.data()
             }
           });
+          // console.log(this.state)
       });
     }
         this.setState({currentUser:userAuth});
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 
-import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-button/custom-button.component';
+import FormInput from '../form-input/form-input.component.jsx';
+import CustomButton from '../custom-button/custom-button.component.jsx';
 
-import {auth, createUserProfileDocument} from '../../firebase/firebase.utilites';
+import {auth, createUserProfileDocument} from '../../firebase/firebase.utilites.jsx';
 
 import './sign-up.styles.scss';
-import { ContextReplacementPlugin } from 'webpack';
 
 class SignUp extends Component {
     constructor(){
@@ -21,19 +20,37 @@ class SignUp extends Component {
     }
 
     handleSubmit = async event => {
-        event.preventDefault()
+        event.preventDefault();
+
         const {displayName, email, password, confirmPassword } = this.state;
 
         if(password !== confirmPassword){
-            alert("passwords do not match")
+            alert("passwords do not match");
             return;
         }
 
         try {
-            const {user} = await auth.createUserWithEmailAndPassword(email, password);
-        
-            ContextReplacementPlugin
+            const {user} = await auth.createUserWithEmailAndPassword(
+                email,
+                password
+            );
+
+           await createUserProfileDocument(user, {displayName});
+           this.setState({
+            displayName:'',
+            email:'',
+            password:'',
+            confirmPassword:''
+           })
+
+        } catch(error){
+            console.error(error)
         }
+    }
+
+    handleChange=(event)=> {
+        const {name, value} = event.target;
+        this.setState({[name]: value});
     }
 
     render(){
@@ -42,7 +59,7 @@ class SignUp extends Component {
             <div className ='sign-up'>
                 <h2 className='title'>I do not have a account</h2>
                 <span> Sign up with your email and password</span>
-                <form className='sign-up-form' onsubmit = {this.handleSubmit}>
+                <form className='sign-up-form'  onSubmit={this.handleSubmit}>
                     <FormInput
                         type='text'
                         name='displayName'
@@ -58,7 +75,7 @@ class SignUp extends Component {
                         onChange = {this.handleChange}
                         label ='email'
                         required
-                    />                    
+                    />
                     <FormInput
                         type='password'
                         name='password'
@@ -66,7 +83,7 @@ class SignUp extends Component {
                         onChange = {this.handleChange}
                         label ='password'
                         required
-                    />                    
+                    />
                     <FormInput
                         type='password'
                         name='confirmPassword'
@@ -74,13 +91,13 @@ class SignUp extends Component {
                         onChange = {this.handleChange}
                         label ='Confirm Password'
                         required
-                    />                
+                    />
                     <CustomButton type='submit'>SIGN UP</CustomButton>
                 </form>
-            
-            
+
+
             </div>
-    
+
 
         )
     }
