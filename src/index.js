5f0a81b75a10896bcd94ce2fd5d4bb37e4e0e8b6
Aaron Waguespack <46909858+Aaron-Waguespack@users.noreply.github.com>
2021-02-26 12:56:13 -0700

    added redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+
 import { BrowserRouter } from 'react-router-dom';
-
-
+import { Provider } from 'react-redux';
+import store from './redux/store.js'
+import App from './App.jsx';
 
 ReactDOM.render(
+  <Provider store = {store}>    
     <BrowserRouter>
-        <App />
-    </BrowserRouter>,  
+          <App />
+      </BrowserRouter>
+    </Provider>,  
   document.getElementById('app')
 );
